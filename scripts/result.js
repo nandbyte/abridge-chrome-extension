07dec5167d9f21942a1f4ada164122e813e3f62b
zedrex <kabeer.adib.abrar@gmail.com>
2022-05-19 01:25:22 +0600

    Divided original text into chunks

--- scripts/result.js
+++ scripts/result.js
@@ -1,12 +1,45 @@
 chrome.storage.local.get(["original"], (result) => {
-    console.log(result);
-
     // Get the string from local storage
     const originalText = result.original;
 
     // Update original data on the website
     const originalElement = document.getElementById("original-text");
     originalElement.innerText = originalText;
+
+    // Divide text into lines with respective word count
+    const lines = originalText.split(". ");
+
+    const linesWithWordCount = lines.map((line) => {
+        const wordCount = line.split(" ").length;
+        return { wordCount, line };
+    });
+
+    // Attach lines together to create 100-word chunks
+
+    let textChunks = [];
+    let currentChunk = "";
+    let currentWordCount = 0;
+
+    linesWithWordCount.forEach((lineWithCount, index) => {
+        if (currentWordCount + lineWithCount.wordCount <= 100) {
+            currentChunk += lineWithCount.line + ". ";
+            currentWordCount += lineWithCount.wordCount;
+        } else {
+            textChunks.push(currentChunk);
+            currentChunk = lineWithCount.line + ". ";
+            currentWordCount = lineWithCount.wordCount;
+        }
+
+        if (index === linesWithWordCount.length - 1) {
+            textChunks.push(lineWithCount.line + ".");
+        }
+    });
+
+    textChunks.forEach((textChunk) => {
+        console.log(
+            `Word Count - ${textChunk.split(" ").length} : ${textChunk}`
+        );
+    });
 
     // Select the abridge element and set to loading
     const abridgeElement = document.getElementById("abridge-text");
--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,6 +1,4 @@
 const summarizeText = (info, tab) => {
-    console.log("Word " + info.selectionText + " was clicked.");
-
     chrome.storage.local.set({ original: info.selectionText }, () => {
         console.log("Original data saved.");
     });
